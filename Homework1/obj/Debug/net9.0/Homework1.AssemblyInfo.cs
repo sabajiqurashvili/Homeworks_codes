02fed128a3e2e68798efeca5374db61408aed4ce
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-09-16 12:16:40 +0400

    add homework2 file

--- Homework1/obj/Debug/net9.0/Homework1.AssemblyInfo.cs
+++ Homework1/obj/Debug/net9.0/Homework1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef42b82d6065d5c54b069284a46dbaa88a4645ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4156e7d28d6b947a6b2d214fa330fd39d6fe0eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
