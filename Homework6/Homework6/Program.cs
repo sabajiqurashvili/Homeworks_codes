fce2f1a7f9cfc5d26b91b968ceb31eb39a870871
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-09-22 16:51:32 +0400

    N3

--- Homework6/Homework6/Program.cs
+++ Homework6/Homework6/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Homework6;
+﻿using System.Globalization;
+
+namespace Homework6;
 
 class Program
 {
@@ -72,49 +74,80 @@ class Program
         
         //N2
         
-        Dictionary<string,string> contacts = new Dictionary<string,string>();
-        while (true)
-        {
-            Console.WriteLine("enter what you want to do: add | remove | allcontacts | exit ");
-            string input = Console.ReadLine();
-            if (input == "exit")
-            {
-                break;
-            }
-            if (input.ToLower() == "add" )
-            {
-                Console.Write("Enter first name: ");
-                string name = Console.ReadLine();
-                Console.Write("Enter number : ");
-                string number = Console.ReadLine();
-
-                contacts.Add(name, number);
-                Console.WriteLine($"Added contact {name} with number {number}");
-            }
-            else if (input.ToLower() == "remove")
-            {
-                Console.Write("Enter name what you want to remove: ");
-                string name = Console.ReadLine();
-                if (contacts.ContainsKey(name))
-                {
-                    contacts.Remove(name);
-                    Console.WriteLine($"Removed contact {name}");
-                }
-                else
-                {
-                    Console.WriteLine("There is no such contact");
-                }
-                
-            }
-            else if (input.ToLower() == "allcontacts")
-            {
-                foreach (var pair in contacts)
-                {
-                    Console.WriteLine($"{pair.Key} : {pair.Value}");
-                }
-            }
-        }
+        // Dictionary<string,string> contacts = new Dictionary<string,string>();
+        // while (true)
+        // {
+        //     Console.WriteLine("enter what you want to do: add | remove | allcontacts | exit ");
+        //     string input = Console.ReadLine();
+        //     if (input == "exit")
+        //     {
+        //         break;
+        //     }
+        //     if (input.ToLower() == "add" )
+        //     {
+        //         Console.Write("Enter first name: ");
+        //         string name = Console.ReadLine();
+        //         Console.Write("Enter number : ");
+        //         string number = Console.ReadLine();
+        //
+        //         contacts.Add(name, number);
+        //         Console.WriteLine($"Added contact {name} with number {number}");
+        //     }
+        //     else if (input.ToLower() == "remove")
+        //     {
+        //         Console.Write("Enter name what you want to remove: ");
+        //         string name = Console.ReadLine();
+        //         if (contacts.ContainsKey(name))
+        //         {
+        //             contacts.Remove(name);
+        //             Console.WriteLine($"Removed contact {name}");
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine("There is no such contact");
+        //         }
+        //         
+        //     }
+        //     else if (input.ToLower() == "allcontacts")
+        //     {
+        //         foreach (var pair in contacts)
+        //         {
+        //             Console.WriteLine($"{pair.Key} : {pair.Value}");
+        //         }
+        //     }
+        // }
         
+        //N3
+
+        Console.Write("Enter length of array: ");
+        int length = int.Parse(Console.ReadLine());
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine("Enter element: ");
+            array[i] = int.Parse(Console.ReadLine());   
+        }
+       
+
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+            
+        }
+
+        Console.WriteLine();
+        
+
+       var groups = array.GroupBy(x => x);
+       foreach (var x in groups)
+       {
+           Console.WriteLine($"{x.Key} appears {x.Count()} times in the array sum {x.Sum()}"); 
+       }
+        
+
+        
+
+
 
     }
 }
