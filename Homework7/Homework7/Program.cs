7fa67af2b657d9f9d67d9acbc6c399daa9ae9553
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-09-25 12:56:12 +0400

    N3

--- Homework7/Homework7/Program.cs
+++ Homework7/Homework7/Program.cs
@@ -14,10 +14,22 @@ class Program
         
         //N2
         
-        var input = new Char[] { '@', '@', '@', '@', '@' };
-        var res = input.All(x => x == input[Array.IndexOf(input, x)+1]);
-        Console.WriteLine(res ? "Yes" : "No");
-       
+        // var input = new Char[] { '@', '@', '@', '@', '@' };
+        // var res = input.All(x => x == input[Array.IndexOf(input, x)+1]);
+        // Console.WriteLine(res ? "Yes" : "No");
+        //
+        
+        //N3
+
+        Console.WriteLine("Enter wins : ");
+        int wins = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter los : ");
+        int los = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Draws : ");
+        int draws = int.Parse(Console.ReadLine());
+
+        var res = wins * 3 + draws;
+        Console.WriteLine(res);
 
     }
 }
