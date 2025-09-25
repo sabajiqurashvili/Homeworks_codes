4f05be8b21360118907eccee16a27c25f9a737d2
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-09-25 12:36:51 +0400

    add program cs file

--- Homework7/Homework7/Program.cs
+++ Homework7/Homework7/Program.cs
@@ -5,12 +5,16 @@ class Program
     static void Main(string[] args)
     {
         //N1
-        Console.WriteLine("Enter Radius : ");
-        var radius = int.Parse(Console.ReadLine());
-        var s1 = (2 * radius) * (2 * radius);
-        var s2 = 2 * radius * radius;
-        var dif = s1 - s2;
-        Console.WriteLine($"dif = {dif} while radius = {radius}");
+        // Console.WriteLine("Enter Radius : ");
+        // var radius = int.Parse(Console.ReadLine());
+        // var s1 = (2 * radius) * (2 * radius);
+        // var s2 = 2 * radius * radius;
+        // var dif = s1 - s2;
+        // Console.WriteLine($"dif = {dif} while radius = {radius}");
+        
+        //N2
+        
+        var input = [ “@”,“@”,“@”,“@”,“@”,“@”]
         
     }
 }
