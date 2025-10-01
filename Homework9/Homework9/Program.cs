b21a632f55eb1d69d5d899e3cedadb33a802dd5b
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-10-02 02:20:21 +0400

    N1

--- Homework9/Homework9/Program.cs
+++ Homework9/Homework9/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Employee employee1 = new Employee(
+            "saba", "jikurashvili", 21, "manager", new int[] { 8, 8, 8, 8, 8, 0, 0 }
+            );
+        Employee employee2 = new Employee(
+            "bidzina", "tabagari", 62, "oper", new int[] { 12, 12, 12, 12, 12, 12, 5 }
+        );
+        // Console.WriteLine(employee1.WeekIncome());
+        // Console.WriteLine(employee1.EmloyeeInformation());
+
+        Company company = new Company(true);
+        company.addEmployee(employee1);
+        company.addEmployee(employee2);
+        company.EmployeesInformation();
+        Console.WriteLine(company.govermentTaxWeekly());
+        
+
     }
 }
