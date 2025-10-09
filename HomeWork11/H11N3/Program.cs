823a9c088ee865215f31d18e7ed50a814a8b9058
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-10-09 12:05:53 +0400

    add H11N3

---
+++ HomeWork11/H11N3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace H11N3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\Users\Jikura\Desktop\davalebebi\Homeworks\HomeWork11\H11N3\file.xml";
+        List<string> list = SplitStringEqual("programming", 2);
+        var elements = list.Select((item, i) => new XElement(item, $"string {i + 1}"));
+
+        XDocument doc = new XDocument
+        (
+            new XElement("strings", elements)
+        ); 
+        doc.Save(path);
+        
+    }
+
+    static List<string> SplitStringEqual(string str, int n)
+    {
+        List<string> result = new List<string>();
+        int quanity = (int)Math.Ceiling(str.Length / (double)n);
+
+
+        for (int i = 0; i < str.Length; i += quanity)
+        {
+            result.Add(str.Substring(i, Math.Min(quanity, str.Length - i)));
+        }
+
+        return result;
+    }
+}
