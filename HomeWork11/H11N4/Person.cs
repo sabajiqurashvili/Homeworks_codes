fbe7e3bfb1d04dccc3526ccaabcb44a887ac440e
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-10-09 12:41:06 +0400

    H11N4

---
+++ HomeWork11/H11N4/Person.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace H11N4;
+
+public class Person
+{
+    public string CurrentDate { get; set; }
+    public string Birthday { get; set; }
+    
+}
--- HomeWork11/H11N4/Program.cs
+++ HomeWork11/H11N4/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace H11N4;
+﻿using System.Text.Json;
+using System.Xml;
+
+namespace H11N4;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        string jsonstring =
+            File.ReadAllText(@"C:\Users\Jikura\Desktop\davalebebi\Homeworks\HomeWork11\H11N4\Birthday.json");
+        Person person = JsonSerializer.Deserialize<Person>(jsonstring);
+        DateTime currentDate = DateTime.Parse(person.CurrentDate);
+        DateTime birthday = DateTime.Parse(person.Birthday);
+        Console.WriteLine((birthday-currentDate).Days);
+
+       
     }
 }
