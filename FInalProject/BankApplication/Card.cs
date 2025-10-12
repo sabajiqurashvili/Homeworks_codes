574b7e374a7fa04a841d927660d84b038b04484c
sabajiqurashvili <sabajiqurashvili8@gmail.com>
2025-10-12 16:12:54 +0400

    add final projects files

---
+++ FInalProject/BankApplication/Card.cs
@@ -0,0 +1,173 @@
+using System.Security.Cryptography;
+using System.Text.Json;
+using Serilog;
+
+namespace BankApplication;
+
+public class Card
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    private string cardNumber;
+    private string expirationDate;
+    private string cvc;
+
+    private int pinCode = 1234;
+    private double GELAmount = 0;
+    private double EURAmount = 0;
+    private double USDAmount = 0;
+    
+    
+
+    public string CardNumber
+    {
+        get { return cardNumber; }
+        set
+        {
+            if (value.Length == 16 && long.TryParse(value, out _))
+                cardNumber = value;
+            else
+            {
+                Log.Error("Invalid card number | number size must be 16 characters");
+                throw new ArgumentException("Invalid card number | number size must be 16 characters");
+            }
+              
+            
+            
+        }
+    }
+
+    public string ExpirationDate
+    {
+        get { return expirationDate; }
+        set
+        {
+            if (value.Length == 5 && value[2] == '/' && int.TryParse(value[..2], out _) && int.TryParse(value[3..], out _))
+                expirationDate = value;
+            else
+            {
+                Log.Error("Invalid Expiration Date");
+                throw new ArgumentException("Invalid Expiration Date. Use format 'MM/YY'");
+            }
+            
+        }
+    }
+
+    public string Cvc
+    {
+        get { return cvc; }
+        set
+        {
+            if (value.Length == 3 && int.TryParse(value, out _)) 
+                cvc = value;
+            else
+            {
+                Log.Error("Invalid CVC number");
+                throw new ArgumentException("Invalid CVC number | numbers length must be 3 characters");  throw new ArgumentException("Invalid CVC number | numbers length must be 3 characters");
+            }
+              
+            
+        }
+    }
+    
+
+    public string CardDetails()
+    {
+        return $"FirstName : {FirstName}\n" +
+               $"LastName : {LastName}\n" +
+               $"CardDetails\n" +
+               $"CardNumber : {CardNumber}\n" +
+               $"ExpirationDate : {ExpirationDate}\n" +
+               $"Cvc : {Cvc}";
+    }
+
+    public int GetPinCode()
+    {
+        return pinCode;
+    }
+
+    public string CheckBalance()
+    {
+        Log.Information("{FirstName} {LastName} Checked balance", FirstName, LastName);
+        Log.Information("balance :\n" +
+                        "GEL : {GelAmount}\n" +
+                        "EUR : {EURAmount}\n" +
+                        "USD : {USDAmount}");
+        return $"Balance :\n" +
+               $" GEL : {GELAmount}\n" +
+               $" EUR : {EURAmount}\n" +
+               $" USD : {USDAmount}\n";
+    }
+
+    public void Withdraw(double amount)
+    {
+        
+        if (amount > GELAmount)
+        {
+            Log.Information("{FirstName} {LastName} Withdrawn {Amount} GEL ",FirstName, LastName, amount);
+            GELAmount = GELAmount-amount; 
+        }
+        else
+        {
+            Log.Error("You dont have enough gel amount");
+            Console.WriteLine("You dont have enough gel amount");
+        }
+    }
+
+    public void AddGELAmount(double amount)
+    {
+        
+        GELAmount += amount;
+        Log.Information("{FirstName} {LastName} added {Amount} GEl amount",FirstName,LastName,amount);
+    }
+
+    public void ChangePinCode(int newpinCode)
+    {
+        if (pinCode.ToString().Length == 4)
+        {
+           pinCode = newpinCode;
+            Log.Information("{FirstName} {LastName} changed pin code to {PinCode}",FirstName, LastName,pinCode);
+        }
+        else
+        {
+            Console.WriteLine("pin code must be 4 digits");
+            Log.Error("pin code must be 4 digits");
+        }
+    }
+
+    public void ChangeAmount(string from, string to, double amountInGEL)
+    {
+        double eur = 0.32;
+        double usd = 0.37;
+        switch (from,to)
+        {
+            case ("GEL","EUR"):
+                GELAmount -= amountInGEL;
+                EURAmount+= amountInGEL*eur;
+                Log.Information("{FirstName} {LastName} converted from GEL to EUR | {AmountInGEL} GEL",FirstName,LastName,amountInGEL);
+                break;
+            case ("GEL","USD"):
+                GELAmount -= amountInGEL;
+                USDAmount+= amountInGEL*usd;
+                Log.Information("{FirstName} {LastName} converted from GEL to USD | {AmountInGEL} GEL",FirstName,LastName,amountInGEL);
+                break;
+            case ("EUR","GEL"):
+                EURAmount -= amountInGEL*eur;
+                GELAmount+= amountInGEL/eur;
+                Log.Information("{FirstName} {LastName} converted from EUR to GEL | {AmountInGEL} GEL",FirstName,LastName,amountInGEL);
+                break;
+            case ("USD","GEL"):
+                USDAmount -= amountInGEL*usd;
+                GELAmount += amountInGEL/usd;
+                Log.Information("{FirstName} {LastName} converted from USD to GEL | {AmountInGEL} GEL",FirstName,LastName,amountInGEL);
+                break;
+            default:
+                Console.WriteLine("Invalid input");
+                Log.Error("Invalid input for converting");
+                break;
+            
+            
+        }
+    }
+}
---
+++ FInalProject/BankApplication/Program.cs
@@ -0,0 +1,91 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+
+namespace BankApplication;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(
+                new JsonFormatter(),
+                @"C:\Users\Jikura\Desktop\davalebebi\Homeworks\FInalProject\BankApplication\logs.json",
+                rollingInterval: RollingInterval.Day, // optional
+                shared: false // safer than true for single process
+            )
+            .CreateLogger();
+
+
+        Card card = new Card();
+        Console.WriteLine("Enter Name :");
+        string name = Console.ReadLine();
+        card.FirstName = name;
+        Console.WriteLine("Enter lastName :");
+        string lastName = Console.ReadLine();
+        card.LastName = lastName;
+        Console.WriteLine("Enter card number: ");
+        string cardNumber = Console.ReadLine();
+        try
+        {
+            card.CardNumber = cardNumber;
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine($"{e.Message} | {e.StackTrace}");
+            throw;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Unecpected exception: " + e.Message);
+        }
+
+        Console.WriteLine("Enter expiration date : ");
+        string expirationDate = Console.ReadLine();
+        try
+        {
+            card.ExpirationDate = expirationDate;
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine($"{e.Message} | {e.StackTrace}");
+            throw;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Unecpected exception: " + e.Message);
+        }
+
+        Console.WriteLine("Enter cvc: ");
+        string cvc = Console.ReadLine();
+        try
+        {
+            card.Cvc = cvc;
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine($"{e.Message} | {e.StackTrace}");
+            throw;
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Unecpected exception: " + e.Message);
+        }
+
+        Console.WriteLine("Enter pin code: ");
+        int pincode = int.Parse(Console.ReadLine());
+        if (pincode == card.GetPinCode())
+        {
+            Console.WriteLine(card.CardDetails());
+            Log.Information("card details {@card}", card);
+        }
+        else
+        {
+            Console.WriteLine("Invalid pin code");
+            Log.Information("invalid pin code");
+        }
+
+        Log.CloseAndFlush();
+    }
+}
